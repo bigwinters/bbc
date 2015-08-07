fed47b2ca012d59be4687d62f62c6d4a50d29d8d
Daniel Winters <danielwinters@live.com>
2015-08-07 21:48:03 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,60 @@
+    var totalWords = 203;
+                var info = {poker: ["I used to work in casino",6]};
+                var displayed = [];
+                function updateWordCount(diff,pos){
+                    var change=0;
+                    switch(diff){
+                        case "around": change = 43;
+                        break;
+                        case "changing": change = 26;
+                        break;
+                        case "engagement": change = 37;
+                        break;
+                        case "journalistic": change = 13;
+                        break;
+                        case "intact": change = 21;
+                        break;
+                        case "wish": change = 26;
+                        break;
+                        case "my": change = 4;
+                        break;
+                        default:
+                    }
+                    if (pos){totalWords += change;}
+                    else {totalWords -= change;}
+                    if (totalWords >= 250){return false;}
+                    $("#wordcount").html("Word Count = " + totalWords);
+                }
+
+                function displaySecondary(tar){
+                    if (alreadyDisplayed(tar) === true){return;}
+                    if (updateWordCount(tar,true) === false){return;} else
+                        {
+                            tar = "#" + tar;
+                            $(tar).fadeIn();
+                        }
+                }
+
+                function alreadyDisplayed (sec){
+                    for(var i=0;i<displayed.length+1;i++){
+                        if (displayed[i]===sec){ return true;}
+                    }
+                    displayed.push(sec);
+                    return false;
+                }
+
+                function notDisplayed(not){
+                    for (var x=0;x<displayed.length+1;x++){
+                        if (displayed[x]==not){displayed.splice(x,1);}
+                    }
+                }
+
+                $(document).ready( function(){
+                    updateWordCount(0);
+                    $('.secondary').click(function(){
+                        var currentID = $(this).attr('id');
+                        updateWordCount(currentID,false);
+                        $(this).fadeOut();
+                        notDisplayed(currentID);
+                    });
+                });
